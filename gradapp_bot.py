c9caae8a2285d1fb9d75bd2ac08f2a0e6aa6169f
xjasonlyu <xjasonlyu@gmail.com>
2023-02-20 18:48:44 +0800

    Refactor: add helper

--- gradapp_bot.py
+++ gradapp_bot.py
@@ -8,6 +8,7 @@ import time
 import traceback
 import typing
 from datetime import datetime
+from functools import cached_property
 from urllib.parse import quote
 from zoneinfo import ZoneInfo
 
@@ -27,26 +28,63 @@ def wait(n: float):
     return decorator
 
 
-def no_exception(v: typing.Any):
-    def decorator(call):
-        def wrapper(*args, **kwargs):
-            try:
-                return call(*args, **kwargs)
-            except Exception as e:
-                logging.warning('{function}(...) => {exception}'.
-                                format(function=call.__name__, exception=e))
-                return v
-        return wrapper
-    return decorator
+class Helper1P3A:
 
+    def __init__(self, token: str, device_id: str):
+        self.authorization = token
+        self.device_id = device_id
+        self.session = requests.Session()
+        self.useragent = '{UA}/0 CFNetwork/1404.0.5 Darwin/22.3.0' \
+            .format(UA=quote("一亩三分地"))
 
-def get_gradapp_threads(last_tid: int = 0) -> list[dict]:
-    with requests.Session() as session:
+    @cached_property
+    def headers(self) -> dict:
+        return {
+            'accept': 'application/json, text/plain, */*',
+            'accept-encoding': 'gzip, deflate, br',
+            'accept-language': 'en-US,en;q=0.9',
+            'content-type': 'application/json',
+            'device-id': self.device_id,
+            'user-agent': self.useragent,
+            'authorization': self.authorization,
+        }
+
+    @cached_property
+    def options(self) -> dict:
+        """
+        curl 'https://api.1point3acres.com/api/types/164/options'
+            -H ':authority: api.1point3acres.com'
+            -H 'accept: */*'
+            -H 'content-type: application/json'
+            -H 'user-agent: %E4%B8%80%E4%BA%A9%E4%B8%89%E5%88%86%E5%9C%B0/0 CFNetwork/1404.0.5 Darwin/22.3.0'
+            -H 'device-id: 00000000-0000-0000-0000-000000000000'
+            -H 'accept-language: en-US,en;q=0.9'
+            -H 'authorization: eyJhbG...'
+            -H 'accept-encoding: gzip, deflate, br' --compressed
+        """
+
+        with self.session.get(
+                url='https://api.1point3acres.com/api/types/164/options',
+                headers=self.headers) as r:
+            r.raise_for_status()
+            data = r.json()
+
+        assert data['errno'] == 0
+
+        # pre-defined options
+        return data['options']
+
+    def _find_option_by_id(self, option_id: int) -> dict:
+        for option in self.options:
+            if option['optionid'] == option_id:
+                return option
+        return {}
+
+    def get_gradapp_threads(self, last_tid: int = 0) -> typing.Iterable[dict]:
 
         @wait(random.uniform(1, 3))
-        def inline_get_gradapp_threads(pg: int = 1, depth: int = 1) -> list[dict]:
+        def inline_get_gradapp_threads(pg: int = 1, depth: int = 1) -> typing.Iterable[dict]:
             """
-            cURL Example:
             curl 'https://api.1point3acres.com/api/forums/82/threads?ps=20&order=time_desc&includes=images,topic_tag&pg=1'
                 -H ':authority: api.1point3acres.com'
                 -H 'accept: application/json, text/plain, */*'
@@ -57,20 +95,15 @@ def get_gradapp_threads(last_tid: int = 0) -> list[dict]:
                 -H 'accept-language: en-US,en;q=0.9' --compressed
             """
 
-            with session.get(
+            with self.session.get(
                     url='https://api.1point3acres.com/api/forums/82/threads',
+                    headers=self.headers,
                     params={
                         'ps': 20,
                         'order': 'time_desc',
                         'includes': 'images,topic_tag',
                         'pg': pg,
                     },
-                    headers={
-                        'accept': 'application/json, text/plain, */*',
-                        'accept-encoding': 'gzip, deflate, br',
-                        'accept-language': 'en-US,en;q=0.9',
-                        'user-agent': '{UA}/0 CFNetwork/1404.0.5 Darwin/22.3.0'.format(UA=quote("一亩三分地")),
-                    }
             ) as r:
                 r.raise_for_status()
                 data = r.json()
@@ -93,32 +126,66 @@ def get_gradapp_threads(last_tid: int = 0) -> list[dict]:
 
         return inline_get_gradapp_threads()
 
+    def get_gradapp_threads_with_details(self, last_tid: int = 0) -> typing.Iterable[dict]:
+        return (dict(**thread, details=self.get_thread_details(thread['tid']))
+                for thread in self.get_gradapp_threads(last_tid=last_tid))
 
-def extend_threads(threads: typing.Iterable[dict]) -> typing.Iterable[dict]:
-    with requests.Session() as session:
-        @no_exception(v={})
-        @wait(random.uniform(0.5, 2.0))
-        def get_thread_details(tid: int) -> dict:
-            with session.get(
-                    url='https://www.1point3acres.com/bbs/thread-{tid}-1-1.html'.format(tid=tid)) as r:
-                r.raise_for_status()
+    @wait(random.uniform(0.5, 2.0))
+    def get_thread_details(self, tid: int) -> dict:
+        """
+        curl 'https://api.1point3acres.com/api/threads/968936/options'
+            -H ':authority: api.1point3acres.com'
+            -H 'accept: */*'
+            -H 'content-type: application/json'
+            -H 'user-agent: %E4%B8%80%E4%BA%A9%E4%B8%89%E5%88%86%E5%9C%B0/0 CFNetwork/1404.0.5 Darwin/22.3.0'
+            -H 'device-id: 00000000-0000-0000-0000-000000000000'
+            -H 'accept-language: en-US,en;q=0.9'
+            -H 'authorization: eyJhbG...'
+            -H 'accept-encoding: gzip, deflate, br' --compressed
+        """
 
-                return dict((k, v) for k, v in
-                            ((str(row.find('th').text).rstrip(':'), str(row.find('td').text).strip())
-                             for row in BeautifulSoup(r.content, 'html.parser')
-                            .find('table', attrs={'summary': '分类信息'})
-                            .find('tbody')
-                            .find_all('tr'))
-                            if not any(s in v for s in ('隐藏内容', '积分不足', '解锁阅读')))
+        with self.session.get(
+                url='https://api.1point3acres.com/api/threads/{tid}/options'.format(tid=tid),
+                headers=self.headers
+        ) as r:
+            r.raise_for_status()
+            data = r.json()
 
-        return (dict(**thread, details=get_thread_details(thread['tid'])) for thread in threads)
+        assert data['errno'] == 0
+
+        # translate options to details
+        details = {}
+        for option in data['options']:
+            value = str(option['value']).strip(' |')
+            opt = self._find_option_by_id(option['optionid'])
+            if not opt or not value:
+                continue
+            details[opt['title']] = \
+                opt['choices'][value] if opt.get('choices') else value
+        return details
+
+    @wait(random.uniform(0.5, 2.0))
+    def get_thread_details_legacy(self, tid: int) -> dict:
+        # NOTE: this method can be usually blocked by Cloudflare
+        with self.session.get(
+                url='https://www.1point3acres.com/bbs/thread-{tid}-1-1.html'.format(tid=tid)) as r:
+            r.raise_for_status()
+
+            return dict((k, v) for k, v in
+                        ((str(row.find('th').text).rstrip(':'), str(row.find('td').text).strip())
+                         for row in BeautifulSoup(r.content, 'html.parser')
+                        .find('table', attrs={'summary': '分类信息'})
+                        .find('tbody')
+                        .find_all('tr'))
+                        if not any(s in v for s in ('隐藏内容', '积分不足', '解锁阅读')))
 
 
 class GradAppBot:
 
-    def __init__(self, bot_token: str, chat_id: str):
+    def __init__(self, bot_token: str, chat_id: str, helper: Helper1P3A):
         self.bot_token = bot_token
         self.chat_id = chat_id
+        self.helper = helper
         self.chat_description = ''
         self.bot = telegram.Bot(self.bot_token)
 
@@ -175,15 +242,10 @@ class GradAppBot:
 
     async def check_and_push(self):
         last_tid = await self.get_last_tid()
-        threads = get_gradapp_threads(last_tid=last_tid)
-
-        # skip if no threads
-        if len(threads) == 0:
-            logging.info('No new threads found since last tid: {0}.'.format(last_tid))
-            return
+        threads = self.helper.get_gradapp_threads_with_details(last_tid=last_tid)
 
         # extend and iterate threads in ascending order
-        for thread in extend_threads(threads[::-1]):
+        for thread in threads[::-1]:
             # break if update last tid succeeded
             if not await self.set_last_tid(thread['tid']):
                 break
@@ -200,14 +262,19 @@ class GradAppBot:
 def main():
     bot_token = os.getenv('TG_BOT_TOKEN')
     chat_id = os.getenv('TG_CHAT_ID')
+    api_token_1p3a = os.getenv('API_TOKEN_1P3A')
+    device_id_1p3a = os.getenv('DEVICE_ID_1P3A')
 
     if not bot_token \
-            or not chat_id:
+            or not chat_id \
+            or not api_token_1p3a \
+            or not device_id_1p3a:
         logging.info('missing key environment variables.')
         return
 
     try:
-        bot = GradAppBot(bot_token=bot_token, chat_id=chat_id)
+        helper = Helper1P3A(token=api_token_1p3a, device_id=device_id_1p3a)
+        bot = GradAppBot(bot_token=bot_token, chat_id=chat_id, helper=helper)
         bot.async_check_and_push()
     except Exception as e:
         traceback.print_exception(e)
